d66cdd85fe91c55edbfb92de971727f76a7a2b3e
Laurent Leleux <lolo88l@hotmail.com>
2014-11-03 20:53:09 +0100

    feature: #46 improve view and support edition

--- app/scripts/contacts/controllers.js
+++ app/scripts/contacts/controllers.js
@@ -19,20 +19,55 @@ webApp.controller('ContactsController', function ($scope, syncService) {
 	});
 
 	/**
+	 * Show the full data of a contact
+	 * Warning ! $(event.target) != $(this)
+	 */
+	$scope.showFullContact = function (event) {
+		var target = $(event.target);
+		// If menu, return
+		if (target.hasClass('edit')) {
+			return;
+		}
+		// If input, start edit contact
+		if ((target.is('.name span') || target.is('input') || target.hasClass('address-show')) && $(this).hasClass('contact-full')) {
+			$scope.startEditContact(event);
+			return;
+		}
+		// If already fully shown
+		if ($(this).hasClass('contact-full')) {
+			// Stop showing contacts full data
+			$scope.stopFullContacts();
+		} else {
+			// Stop showing other contacts full data
+			$scope.stopFullContacts();
+			// Show full data
+			$(this).addClass('contact-full');
+			// Listen to escape key
+			$('body').on('keydown', _escapeKeyListenerFull);
+		}
+	};
+
+	/**
+	 * Stop showing the full data of the contacts
+	 */
+	$scope.stopFullContacts = function (event) {
+		// Remove full data
+		$('.contact').removeClass('contact-full');
+		// Remove the binding to listen to escape key
+		$('body').off('keydown', _escapeKeyListenerFull);
+	};
+
+	/**
 	 * Start editing a contact
 	 */
 	$scope.startEditContact = function (event) {
-		if ($(this).hasClass('contact-edit')) {
-			// Stop editing other contacts
-			$scope.stopEditContacts();
-		} else {
-			// Stop editing other contacts
-			$scope.stopEditContacts();
-			// Show editor
-			$(this).addClass('contact-edit');
-			// Listen to escape key
-			$('body').on('keydown', _escapeKeyListener);
-		}
+		// Stop listening to editing or show full demands
+		$('body').off('click', '.contact', $scope.showFullContact);
+		$('body').off('keydown', _escapeKeyListenerFull);
+		// Show editor
+		$(event.target).parents('.contact').addClass('contact-edit');
+		// Listen to escape key
+		$('body').on('keydown', _escapeKeyListenerEdit);
 	};
 
 	/**
@@ -42,18 +77,29 @@ webApp.controller('ContactsController', function ($scope, syncService) {
 		// Remove editor
 		$('.contact').removeClass('contact-edit');
 		// Remove the binding to listen to escape key
-		$('body').off('keydown', _escapeKeyListener);
+		$('body').off('keydown', _escapeKeyListenerEdit);
+		// Listen to escape key for full
+		$('body').on('keydown', _escapeKeyListenerFull);
+		// Re-start listening to editing or show full demands
+		$('body').on('click', '.contact', $scope.showFullContact);
 	};
 
-	// Listen to clicks on contact to start edit it
-	$('body').on('click', '.contact', $scope.startEditContact);
+	// Listen to clicks to start edit it or show full data
+	$('body').on('click', '.contact', $scope.showFullContact);
+	$('body').on('click', '.contact .edit', $scope.startEditContact);
+	$('body').on('click', '.contact .ok', $scope.stopEditContacts);
 
 	/**
 	 * Custom listeners
 	 */
-	var _escapeKeyListener = function (event) {
+	var _escapeKeyListenerEdit = function (event) {
 		if (event.which === 27) {
 			$scope.stopEditContacts(event);
+		}
+	};
+	var _escapeKeyListenerFull = function (event) {
+		if (event.which === 27) {
+			$scope.stopFullContacts(event);
 		}
 	};
 
